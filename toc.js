745a5a345753221ebdaee92da454bd463b4f1736
Pascal Hertleif <killercup@gmail.com>
2014-02-07 19:44:29 +0100

    Generated documentation for 82fc67204c55e67936e32026c7e46e5178f5f8a0

---
+++ toc.js
@@ -0,0 +1,209 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "Readme.md",
+  "originalPath": "Readme.md",
+  "name": "index.html",
+  "title": "MC Trevor",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "mc-trevor",
+      "title": "MC Trevor"
+    },
+    {
+      "level": 2,
+      "slug": "get-started",
+      "title": "Get started"
+    },
+    {
+      "level": 2,
+      "slug": "roadmap-ideas",
+      "title": "Roadmap/Ideas"
+    },
+    {
+      "level": 2,
+      "slug": "license",
+      "title": "License"
+    }
+  ]
+},
+{
+  "path": "server.js.html",
+  "originalName": "server.js",
+  "originalPath": "server.js",
+  "name": "server.js.html",
+  "lang": "javascript",
+  "toc": []
+},
+{
+  "path": "gulpfile.coffee.html",
+  "originalName": "gulpfile.coffee",
+  "originalPath": "gulpfile.coffee",
+  "name": "gulpfile.coffee.html",
+  "lang": "coffeescript",
+  "title": "Compile Stuff Using Gulp",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "compile-stuff-using-gulp",
+      "title": "Compile Stuff Using Gulp"
+    },
+    {
+      "level": 2,
+      "slug": "requirements",
+      "title": "Requirements"
+    },
+    {
+      "level": 2,
+      "slug": "constants",
+      "title": "Constants"
+    },
+    {
+      "level": 3,
+      "slug": "vendor-files-from-bower",
+      "title": "Vendor Files from Bower"
+    },
+    {
+      "level": 3,
+      "slug": "paths",
+      "title": "Paths"
+    },
+    {
+      "level": 2,
+      "slug": "tasks",
+      "title": "Tasks"
+    },
+    {
+      "level": 3,
+      "slug": "remove-previously-compiled-files",
+      "title": "Remove previously compiled files"
+    },
+    {
+      "level": 3,
+      "slug": "compile-js-coffee",
+      "title": "Compile JS/Coffee"
+    },
+    {
+      "level": 3,
+      "slug": "concat-minify-files-from-bower",
+      "title": "Concat/Minify Files from bower"
+    },
+    {
+      "level": 3,
+      "slug": "process-html-files-using-lodash-underscore-templates",
+      "title": "Process HTML files using lodash/underscore templates"
+    },
+    {
+      "level": 3,
+      "slug": "build-task",
+      "title": "Build task"
+    },
+    {
+      "level": 3,
+      "slug": "combined-build-watch-task",
+      "title": "Combined build/watch task"
+    }
+  ]
+},
+{
+  "path": "client/js/app.coffee.html",
+  "originalName": "app.coffee",
+  "originalPath": "client/js/app.coffee",
+  "name": "app.coffee.html",
+  "lang": "coffeescript",
+  "title": "MC Trevor Frontend",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "mc-trevor-frontend",
+      "title": "MC Trevor Frontend"
+    },
+    {
+      "level": 2,
+      "slug": "dom",
+      "title": "DOM"
+    },
+    {
+      "level": 2,
+      "slug": "current-page",
+      "title": "Current Page"
+    },
+    {
+      "level": 2,
+      "slug": "helpers",
+      "title": "Helpers"
+    },
+    {
+      "level": 3,
+      "slug": "reset-editor",
+      "title": "Reset editor"
+    },
+    {
+      "level": 3,
+      "slug": "add-an-alert",
+      "title": "Add an alert"
+    },
+    {
+      "level": 3,
+      "slug": "load-pages",
+      "title": "Load Pages"
+    },
+    {
+      "level": 2,
+      "slug": "select-page",
+      "title": "Select Page"
+    },
+    {
+      "level": 2,
+      "slug": "save-page",
+      "title": "Save Page"
+    },
+    {
+      "level": 2,
+      "slug": "add-page",
+      "title": "Add Page"
+    },
+    {
+      "level": 2,
+      "slug": "init",
+      "title": "Init"
+    }
+  ]
+},
+{
+  "path": "server/start.coffee.html",
+  "originalName": "start.coffee",
+  "originalPath": "server/start.coffee",
+  "name": "start.coffee.html",
+  "lang": "coffeescript",
+  "title": "MC Trevor Server",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "mc-trevor-server",
+      "title": "MC Trevor Server"
+    },
+    {
+      "level": 2,
+      "slug": "paths",
+      "title": "Paths"
+    },
+    {
+      "level": 2,
+      "slug": "index-of-json-files",
+      "title": "Index of JSON files"
+    },
+    {
+      "level": 2,
+      "slug": "get-json-files",
+      "title": "GET JSON files"
+    },
+    {
+      "level": 2,
+      "slug": "write-json-files",
+      "title": "Write JSON files"
+    }
+  ]
+}
+];
